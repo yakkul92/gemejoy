eb701f7f4b202c72ce2f56721db7cfc00f98ea61
EC2 Default User <ec2-user@ip-172-31-19-56.ap-southeast-2.compute.internal>
2023-12-08 01:31:41 +0000

    Initial Setting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import jQuery from "jquery"
+import raty from 'raty-js'
+import "popper.js"
+import "bootstrap"
+import "../stylesheets/application"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
