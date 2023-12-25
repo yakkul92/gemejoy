fab76cb138f7a0d8565aed6edc65602f89cf99e3
EC2 Default User <ec2-user@ip-172-31-28-44.ap-southeast-2.compute.internal>
2023-12-25 03:17:14 +0000

    [fix]css

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,86 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 管理者の作成
-User.create!(
-  email: ENV['ADMIN_EMAIL'],
+#管理者の作成
+admin_user = User.find_or_initialize_by(email: ENV['ADMIN_EMAIL'])
+admin_user.assign_attributes(
   password: ENV['ADMIN_PASSWORD'],
   name: ENV['ADMIN_NAME'],
   introduction: ENV['ADMIN_INTRODUCTION'],
   admin: true
 )
+admin_user.save!
 
 # テストユーザーの作成
 3.times do |n|
-  User.create!(
-    email: "user#{n + 1}@example.com",
+  user = User.find_or_initialize_by(email: "user#{n + 1}@example.com")
+  user.assign_attributes(
     password: "password",
     name: "User #{n + 1}",
     introduction: "これはユーザー #{n + 1} です。"
   )
+  user.save!
+
+  item = Item.find_or_create_by!(
+    name: "商品#{n + 1}",
+    shop_code: "SHOP00#{n + 1}",
+    price: (n + 1) * 1000,
+    genre_id: "T00#{n + 1}",
+    rakuten_url: "https://example.com/item#{n + 1}",
+    image_url: '["no_image.jpg", "image2.jpg", "image3.jpg"]',
+    caption: "これは商品 #{n + 1} です。"
+  )
+  # アイテムがすでに存在する場合、そのアイテムを取得
+  existing_item = Item.find_by(name: item.name, shop_code: item.shop_code)
+  item = existing_item if existing_item
+  item.save!
+
+  # レビューデータが存在しない場合のみ新しいレビューデータを作成
+unless Review.exists?(user_id: user.id, item_id: item.id)
+  # レビューデータの作成
+  review = Review.find_or_initialize_by(user_id: user.id, item_id: item.id)
+  review.assign_attributes(
+    comment: "これは商品 #{item.id} へのレビューです。",
+    star: rand(1..5),
+    is_active: true
+  )
+
+  if review.new_record?
+    review.save!
+  else
+    # 既に存在する場合は更新
+    review.update!(
+      comment: "これは商品 #{item.id} への更新済みレビューです。",
+      star: rand(1..5),
+      is_active: true
+    )
+  end
+
+  # レビューコメントの作成
+  2.times do
+    ReviewComment.find_or_create_by(
+      content: "これはレビューへのコメントです。",
+      user_id: User.all.sample.id,
+      review_id: review.id
+    )
+
+    # いいね（レビューに対する）
+    FavoriteReview.find_or_create_by(
+      user_id: User.all.sample.id,
+      review_id: review.id
+     )
+
+     # いいね（商品に対する）
+     FavoriteItem.find_or_create_by(
+       user_id: User.all.sample.id,
+       item_id: item.id
+     )
+     end
+  end
 end
 
-  Genre.create([
+#ジャンルの作成
+Genre.create([
   { id: "567170", name: "ゲーミングキーボード" },
   { id: "565171", name: "ゲーミングマウス" },
   { id: "552391", name: "ゲーミングマウスパッド" },
@@ -34,9 +94,10 @@ end
   { id: "110105", name: "ゲーミングモニター" },
   { id: "211549", name: "ゲームパッド" },
   # 他のジャンルも追加可
-  ])
+])
 
-  Tag.create([
+# タグの作成
+Tag.create([
   { name: "logicool" },
   { name: "Razer" },
   { name: "ELECOM" },
@@ -58,52 +119,5 @@ end
   { name: "BenQ" },
   { name: "SENNHEISER" },
   # 他のジャンルも追加可
-  ])
-
-# テストアイテムの作成
-3.times do |n|
-  item = Item.create!(
-    name: "商品#{n + 1}",
-    shop_code: "SHOP00#{n + 1}",
-    price: (n + 1) * 1000,
-    genre_id: "T00#{n + 1}",
-    rakuten_url: "https://example.com/item#{n + 1}",
-    image_url: '["no_image.jpg", "image2.jpg", "image3.jpg"]',
-    caption: "これは商品 #{n + 1} です。"
-  )
-
-  # 既存のレビューがない場合のみ新しいレビューを作成
-  unless Review.exists?(item_id: item.id)
-    # レビューの作成
-    review = Review.create!(
-      user_id: User.all.sample.id,
-      item_id: item.id,
-      comment: "これは商品 #{item.id} へのレビューです。",
-      star: rand(1..5),
-      is_active: true
-    )
-
-    # レビューコメントの作成
-    2.times do
-      ReviewComment.create!(
-        content: "これはレビューへのコメントです。",
-        user_id: User.all.sample.id,
-        review_id: review.id
-      )
-
-      # いいね（レビューに対する）
-      FavoriteReview.create!(
-        user_id: User.all.sample.id,
-        review_id: review.id
-      )
-
-      # いいね（商品に対する）
-      FavoriteItem.create!(
-        user_id: User.all.sample.id,
-        item_id: item.id
-      )
-    end
-  end
-end
-
+])
 
